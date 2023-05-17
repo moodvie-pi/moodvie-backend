eb83e6c003ee67f9fe0bf27d9f1a6f5c8936c1bb
Rodrigo S Passos <rodrigos.p.2016rsp@gmail.com>
2023-05-17 18:25:36 -0300

    adding movie genre class

---
+++ src/main/java/br/com/moodvie/domain/movie/MovieGenreTMDB.java
@@ -0,0 +1,15 @@
+package br.com.moodvie.domain.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MovieGenreTMDB {
+    Integer id;
+    String name;
+}
