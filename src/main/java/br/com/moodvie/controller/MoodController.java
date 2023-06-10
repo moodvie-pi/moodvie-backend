fcc052a0ee4ef297c4cafeb6fe4775942229d9f6
Rodrigo S Passos <rodrigos.p.2016rsp@gmail.com>
2023-06-10 14:29:59 -0300

    criação de endpoint para atribuir filme/série a um humor

--- src/main/java/br/com/moodvie/controller/MoodController.java
+++ src/main/java/br/com/moodvie/controller/MoodController.java
@@ -1,6 +1,12 @@
 package br.com.moodvie.controller;
 
+import br.com.moodvie.form.MoodForm;
+import br.com.moodvie.services.MoodService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/mood")
 @AllArgsConstructor
 public class MoodController {
+    private final MoodService moodService;
+    @PostMapping()
+    public ResponseEntity<?> saveMood(@RequestBody MoodForm moodForm){
+        moodService.create(moodForm);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
