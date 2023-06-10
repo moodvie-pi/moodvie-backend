1b822a9e91d124fe46f6425ce3fc1d1f8087774f
Davi Pinheiro <davipinheiro935@gmail.com>
2023-06-10 15:42:32 -0300

    corrigindo segurança

--- src/main/java/br/com/moodvie/security/SecurityConfigurations.java
+++ src/main/java/br/com/moodvie/security/SecurityConfigurations.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,7 +25,9 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors().disable()
+                .anonymous().disable()
+                .cors()
+                .and()
                 .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Não autorizado"))
                 .and()
