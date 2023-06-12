2cd8c38f92056103f5162b9b57b6144def048a2f
Davi Pinheiro <davipinheiro935@gmail.com>
2023-06-11 21:02:57 -0300

    adicionando endpoint de achar os conteudos para cada mood

--- src/main/java/br/com/moodvie/controller/MoodController.java
+++ src/main/java/br/com/moodvie/controller/MoodController.java
@@ -1,23 +1,30 @@
 package br.com.moodvie.controller;
 
+import br.com.moodvie.dto.MoodDTO;
 import br.com.moodvie.form.MoodForm;
 import br.com.moodvie.services.MoodService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/mood")
 @AllArgsConstructor
 public class MoodController {
     private final MoodService moodService;
+
     @PostMapping()
-    public ResponseEntity<?> saveMood(@RequestBody MoodForm moodForm){
+    public ResponseEntity<?> saveMood(@RequestBody MoodForm moodForm) {
         moodService.create(moodForm);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<MoodDTO>> findMoods(@RequestParam String lang) {
+        List<MoodDTO> moods = moodService.findMoods(lang);
+        return new ResponseEntity<>(moods, HttpStatus.OK);
     }
 }
